4c3ef6fc6c55e2b2c1b6b55becc745903bcd8888
Shane Ficorilli <sficorilli@sslvpn-96-100.sei.cmu.edu>
2017-04-06 16:06:30 -0400

    Removed 'chef_repo' directory.

--- chef_repo/cookbooks/example/metadata.rb
+++
@@ -1,6 +0,0 @@
-name 'example'
-description 'An example cookbook'
-maintainer 'Example maintainer'
-maintainer_email 'maintainer@example.com'
-license 'Apache v2'
-version '1.0.0'
--- chef_repo/cookbooks/example/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-# This is a Chef recipe file. It can be used to specify resources which will
-# apply configuration to a server.
-
-log "Welcome to Chef, #{node["example"]["name"]}!" do
-  level :info
-end
-
-# For more information, see the documentation: https://docs.chef.io/recipes.html
