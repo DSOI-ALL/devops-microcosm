5bcadbb02e03bbea8cfc81275c8c76ed66231605
Shane Tyler Ficorilli <sficorilli@numa-vpn-10-64-201-51.ad.sei.cmu.edu>
2019-07-05 11:33:33 -0400

    Modified manual code deploy script to match what's used in CloudFormation template.

--- AWS/resources/addCodeDeploytoEC2Image.sh
+++ AWS/resources/addCodeDeploytoEC2Image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-# For Tomcat AMI from marketplace | Linux/Unix, Ubuntu 16.04 | 64-bit (x86) Amazon Machine Image (AMI) 
+# For Tomcat AMI from marketplace | Linux/Unix, Ubuntu 16.04 | 64-bit (x86) Amazon Machine Image (AMI)
 # Debug with logs at /var/log/cloud-init-output.log
 # TODO "When a user data script is processed, it is copied to and executed from a directory in /var/lib/cloud. The script is not deleted after it is run. Be sure to delete the user data scripts from /var/lib/cloud before you create an AMI from the instance. Otherwise, the script will exist in this directory on any instance launched from the AMI and will be run when the instance is launched."
 apt-get -y update
@@ -17,3 +17,4 @@ python awslogs-agent-setup.py -n -r REGION -c s3://aws-codedeploy-us-east-2/clou
 mkdir -p /var/awslogs/etc/config
 cp codedeploy_logs.conf /var/awslogs/etc/config/
 service awslogs restart
+service codedeploy-agent start
